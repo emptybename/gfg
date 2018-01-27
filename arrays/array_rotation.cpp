ed36d91da4a325f4678fa3c5035bedab1d37f2f8
headsshot <mayankankit888@gmail.com>
2018-01-27 17:00:23 +0530

    Add solution for Array rotation

---
+++ arrays/array_rotation.cpp
@@ -0,0 +1,66 @@
+// Rotate array by d elememts.
+// https://www.geeksforgeeks.org/array-rotation/
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+void PrintArray(int ar[], int n){
+	for(int i = 0;i < n; i++)cout<<ar[i]<<" ";
+	cout<<endl;
+}
+
+void RotateArraySolution1(int array_to_rotate[], int n, int d){
+	//time complexity O(n), space complexity O(d)
+	int temp_array[d];
+	for(int i = 0;i < d;i++)temp_array[i] = array_to_rotate[i];
+	//shift array leftward by d
+	for(int i = 0;i+d < n;i++)array_to_rotate[i] = array_to_rotate[i+d];
+	for(int i = 0;i < d;i++)array_to_rotate[n-d+i] = temp_array[i];
+	//for(int i = 0; i < n;i++)cout<<array_to_rotate[i]<<" ";
+	//PrintArray(array_to_rotate, n);
+}
+
+void RotateArraySolution2(int array_to_rotate[], int n, int d){
+	// time complexity O(nd), space complexity O(1)
+	for(int i = 0;i < d; i++){
+		int temp = array_to_rotate[0];
+		for(int j = 1; j < n;j++)array_to_rotate[j-1] = array_to_rotate[j];
+		array_to_rotate[n-1] = temp;
+	}
+	//PrintArray(array_to_rotate, n);
+}
+
+int Gcd(int a, int b){
+	if(b == 0)return a;
+	return Gcd(b, a%b);
+}
+
+void RotateArraySolution3(int array_to_rotate[], int n, int d){
+	int shift_val = Gcd(n,d);
+	for(int i = 0;i < shift_val;i++){
+		int temp = array_to_rotate[i];
+		int j = i;
+		while(1){
+			int k = j+d;
+			if(k >= n)k = k-n;
+			if(k == i) break;
+			array_to_rotate[j] = array_to_rotate[k];
+			j = k;
+		}
+		array_to_rotate[j] = temp;
+	}
+}
+
+int main(){
+	int n;
+	cin>>n;
+	int array_to_rotate[n];
+	for(int i = 0;i < n;i++)cin>>array_to_rotate[i];
+	int d;
+	cin>>d;
+	//RotateArraySolution1(array_to_rotate,n,d);
+	//RotateArraySolution2(array_to_rotate,n,d);
+	RotateArraySolution3(array_to_rotate, n, d);
+	PrintArray(array_to_rotate, n);
+}
+
