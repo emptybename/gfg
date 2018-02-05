226ecae884aed0e60a6d4619e2ec8482a42d48d5
mayank shivhare <mayankankit888@gmail.com>
2018-02-05 23:46:48 +0530

    count rotation divisible by 8

---
+++ arrays/arrayRotation/cout_rotation_divisible_by_8.cpp
@@ -0,0 +1,36 @@
+//https://www.geeksforgeeks.org/count-rotations-divisible-8/
+
+#include<iostream>
+#define f(l,n) for(int i = l;i < n;i++)
+#define max 10000
+using namespace std;
+
+int getCount(string s){
+	int len = s.length();
+	int count = 0;
+	if(len == 1){
+		if(s[0] == '8')
+			return 1;
+		else
+			return 0;
+	}
+	if(len == 2){
+		if(((s[0]-'0')*10+(s[1]-'0'))%8 == 0)count++;
+		if(((s[1]-'0')*10+(s[0]-'0'))%8 == 0)count++;
+		return count;
+	}
+	int sum = ((s[0] - '0')*10 + (s[1] - '0'))*10 + (s[2] - '0');
+	if(sum%8 == 0)count++;
+	for(int i = 1;i < len;i++){
+		sum = sum*10 + s[(i+2)%len] - '0';
+		sum = sum % 1000;
+		if(sum % 8 == 0)count++;
+	}
+	return count;
+}
+
+int main(){
+	string s;
+	cin>>s;
+	cout<<getCount(s);
+}
