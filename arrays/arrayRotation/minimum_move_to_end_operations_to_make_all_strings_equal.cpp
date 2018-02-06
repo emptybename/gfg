5bf548049876fb6ba631c47187fb47c849506eff
mayank shivhare <mayankankit888@gmail.com>
2018-02-06 21:47:59 +0530

    update minimum_move_to_end_operations_to_make_all_strings_equal

--- arrays/arrayRotation/minimum_move_to_end_operations_to_make_all_strings_equal.cpp
+++ arrays/arrayRotation/minimum_move_to_end_operations_to_make_all_strings_equal.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #define f(l,n) for(int i = l;i < n;i++)
 using namespace std;
+int n;
+int ar[1000][1000];
 
 int numberOfRotation(string a, string b){
 	int aLen = a.length();
@@ -13,22 +15,7 @@ int numberOfRotation(string a, string b){
 
 }
 
-
-int main(){
-	int n;
-	cin>>n;
-	string s[n];
-	f(0,n)cin>>s[i];
-	int ar[n][n];
-	ar[0][0] = 0;
-	int sLen = n > 0 ? s[0].length() : 0; 
-	f(1,n){
-		ar[0][i] = numberOfRotation(s[0],s[i]);
-		if(ar[0][i] == -1){
-			cout<<"-1"<<endl;
-			return 0;
-		}
-	}
+void makeEveryRotation(int sLen){
 	f(1,n){
 		for(int j = 0;j < n;j++){
 			if(i == j)
@@ -41,10 +28,9 @@ int main(){
 			}
 		}
 	}
-	f(0,n){
-		for(int j = 0;j<n;j++)cout<<ar[i][j]<<" ";
-		cout<<endl;
-	}
+}
+
+int minRotationCount(){
 	int min = INT_MAX;
 	f(0,n){
 		int temp = 0;
@@ -53,5 +39,28 @@ int main(){
 		}
 		if(temp < min)min = temp;
 	}
-	cout<<min<<endl;
+	return min;
+}
+
+int main(){
+	//int n;
+	cin>>n;
+	string s[n];
+	f(0,n)cin>>s[i];
+	//int ar[n][n];
+	ar[0][0] = 0;
+	int sLen = n > 0 ? s[0].length() : 0; 
+	f(1,n){
+		ar[0][i] = numberOfRotation(s[0],s[i]);
+		if(ar[0][i] == -1){
+			cout<<"-1"<<endl;
+			return 0;
+		}
+	}
+	makeEveryRotation(sLen);
+	f(0,n){
+		for(int j = 0;j<n;j++)cout<<ar[i][j]<<" ";
+		cout<<endl;
+	}
+	cout<<minRotationCount()<<endl;
 }
