b9bd920e2320755ef1ac182cfdc13f1e47c63fe8
mayank shivhare <mayankankit888@gmail.com>
2018-02-06 21:34:47 +0530

    arrayRotation/minimum_move_to_end_operations_to_make_all_strings_equal

---
+++ arrays/arrayRotation/minimum_move_to_end_operations_to_make_all_strings_equal.cpp
@@ -0,0 +1,57 @@
+// https://www.geeksforgeeks.org/minimum-move-end-operations-make-strings-equal/
+#include<iostream>
+#include<string>
+#define f(l,n) for(int i = l;i < n;i++)
+using namespace std;
+
+int numberOfRotation(string a, string b){
+	int aLen = a.length();
+	int bLen = b.length();
+	if(aLen != bLen)return -1;
+	a += a;
+	return a.find(b);
+
+}
+
+
+int main(){
+	int n;
+	cin>>n;
+	string s[n];
+	f(0,n)cin>>s[i];
+	int ar[n][n];
+	ar[0][0] = 0;
+	int sLen = n > 0 ? s[0].length() : 0; 
+	f(1,n){
+		ar[0][i] = numberOfRotation(s[0],s[i]);
+		if(ar[0][i] == -1){
+			cout<<"-1"<<endl;
+			return 0;
+		}
+	}
+	f(1,n){
+		for(int j = 0;j < n;j++){
+			if(i == j)
+				ar[i][j] = 0;
+			else if(j < i){
+				ar[i][j] = sLen - ar[j][i];
+			}
+			else{
+				ar[i][j] = (ar[i-1][j] - ar[i-1][i] + sLen) % sLen;
+			}
+		}
+	}
+	f(0,n){
+		for(int j = 0;j<n;j++)cout<<ar[i][j]<<" ";
+		cout<<endl;
+	}
+	int min = INT_MAX;
+	f(0,n){
+		int temp = 0;
+		for(int j = 0;j < n; j++){
+			temp += ar[j][i];
+		}
+		if(temp < min)min = temp;
+	}
+	cout<<min<<endl;
+}
